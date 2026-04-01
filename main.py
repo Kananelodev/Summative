0ab80ce22521c827db39e8a86ce91b42b43366ae
Kananelo <kamofeec025@student.wethinkcode.co.za>
2026-04-01 16:07:09 +0200

    Well

--- main.py
+++ main.py
@@ -6,15 +6,48 @@ This file contains the implementations for the Summative assessment questions.
 
 # Question 1: parse_pothole_reports
 def parse_pothole_reports(raw_data: list) -> dict:
-    pass
+    new = {}
 
+    for i in raw_data:
+        ok = i.split('|')
+        well = ok[0].split(":")
+        wello = ok[1].split(":")
+
+        new.setdefault(well[1], int(wello[1]))
+    return new
+     
+
+# print(parse_pothole_reports(["STREET:Bree|SEVERITY:3", "STREET:Alice|SEVERITY:8"]))
 # Question 2: taxi_revenue_tracker
 def taxi_revenue_tracker(trips: list) -> dict:
-    pass
+    new = {}
+
+    for trip in trips:
+        driver = trip['driver']
+        amount = trip['fare']
+        new[driver] = new.get(driver, 0) + amount
+    return new
+
+# print(taxi_revenue_tracker([{"driver": "Sipho", "fare": 100},
+#                             {"driver": "Amara", "fare": 200}, 
+#                             {"driver": "Sipho", "fare": 50}]))
 
 # Question 3: electrical_grid_status
 def electrical_grid_status(suburbs: dict) -> list:
-    pass
+    new = []
+
+    for key, value in suburbs.items():
+        well = value
+        if isinstance(well, dict):
+            new.extend(electrical_grid_status(value))
+        
+        elif value == "OFF":
+            new.append(key)
+    
+    return new
+
+# print(electrical_grid_status({"Gauteng": {"Soweto": "OFF", "Sandton": "ON"}, 
+#                               "Western Cape": {"Langa": "OFF"}}))
 
 # Question 4: grant_payment_formatter
 def grant_payment_formatter(beneficiaries: list) -> str:
@@ -22,8 +55,31 @@ def grant_payment_formatter(beneficiaries: list) -> str:
 
 # Question 5: logistics_chain_validator
 def logistics_chain_validator(nodes: list, cargo_weight: int) -> bool:
-    pass
+    well = [i for i in nodes if i > cargo_weight]
+
+    if len(well) < len(nodes):
+        return False
+    else:
+        return True
+
+print(logistics_chain_validator([100, 80, 150], 90))
 
 # Question 6: deep_merge_inventories
 def deep_merge_inventories(store_a: dict, store_b: dict) -> dict:
-    pass
+    
+    new = {}
+    ok = sum()
+    for key, value in store_b.items():
+        for well, va in store_a.items():
+            pass
+        if well in store_a:
+            new[well] = value + va
+        else:
+            new[key] = value
+      
+    # for key, value in store_b.items():
+    #     store_a.setdefault()
+            
+        return new
+
+print(deep_merge_inventories( {"Milk": 2}, {"Milk": 3, "Bread": 1}))
